d3b21276576fd2ffdd7e76dc30801bf5d2f108fc
Febrina Erwinda Putri <febrinap23@gmail.com>
2026-09-20 20:53:28 +0700

    Labwork 01 Prelab

---
+++ lw01/prelab/Main.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<PrintJob> jobs = new ArrayList<>();
+        File file = new File("lw01/prelab/jobs.txt");
+
+        if (!file.exists()) {
+            file = new File("jobs.txt");
+        }
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                String type = scanner.next();
+                String id = scanner.next();
+                int pages = scanner.nextInt();
+
+                if (type.equalsIgnoreCase("MONO")) {
+                    jobs.add(new MonoPrint(id, pages));
+                } else if (type.equalsIgnoreCase("COLOUR")) {
+                    jobs.add(new ColourPrint(id, pages));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File jobs.txt tidak ditemukan!");
+            return;
+        }
+
+        for (PrintJob job : jobs) {
+            System.out.println(job.summary());
+        }
+    }
+}
